e970be3a4091ef3660424f2bc141a32f7b984e94
vishaligt <vishal.igtechso@gmail.com>
2025-09-15 15:30:42 +0530

    Initial commit - clean repo

--- server/index.ts
+++ server/index.ts
@@ -74,7 +74,7 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    //serveStatic(app);
   }
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
